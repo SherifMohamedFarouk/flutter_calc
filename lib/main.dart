4b554e0f4fb232e943bdb2278bbb251fe259315b
Shiro <sherifmohamed2004@gmail.com>
2021-02-28 17:25:08 +0200

    logic added

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
-      primaryColor: Colors.indigo,
-          accentColor: Colors.indigoAccent,
-        brightness: Brightness.light
-    ),
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent,
+        brightness: Brightness.light),
     debugShowCheckedModeBanner: false,
     home: MoneyCalc(),
   ));
@@ -23,11 +22,17 @@ class MoneyCalcState extends State<MoneyCalc> {
   var currencies = ['Dollar', 'Euro', 'Egp'];
   var minmumpadding = 5.0;
   var currentcurrencies = 'Dollar';
-
+  TextEditingController principalController = TextEditingController();
+  TextEditingController rateController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+  String result = '';
 
   @override
   Widget build(BuildContext context) {
-    TextStyle textStyle = Theme.of(context).textTheme.title;
+    TextStyle textStyle = Theme
+        .of(context)
+        .textTheme
+        .title;
     return Scaffold(
       // backgroundColor: Colors.black12,
       // resizeToAvoidBottomPadding: false,
@@ -40,10 +45,11 @@ class MoneyCalcState extends State<MoneyCalc> {
             getImageAsset(),
             Padding(
               padding: EdgeInsets.all(minmumpadding),
-              child: TextField(cursorColor:Colors.white ,
+              child: TextField(
+                cursorColor: Colors.white,
+                controller: principalController,
                 decoration: InputDecoration(
                     labelText: 'Principal',
-
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(minmumpadding))),
               ),
@@ -51,78 +57,88 @@ class MoneyCalcState extends State<MoneyCalc> {
             Padding(
                 padding: EdgeInsets.all(minmumpadding),
                 child: TextField(
+                  controller: rateController,
                   decoration: InputDecoration(
                       labelText: 'Rate of interest ',
                       hintText: 'in  percent',
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(minmumpadding))),
                 )),
-            Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.all(minmumpadding),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              labelText: 'Term',
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                  BorderRadius.circular(minmumpadding))),
-                        )),
-                  ),
-                  Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.all(minmumpadding),
-                        child: DropdownButton<String>(
-                          hint: Text("Select a currencies"),
-                          value: currentcurrencies,
-                          onChanged: (String value) {
-                            setState(() {
-                              currentcurrencies = value;
-                            });
-                          },
-                          items:currencies.map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                        )
+            Row(children: [
+              Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(minmumpadding),
+                    child: TextField(
+                      controller: termController,
+                      decoration: InputDecoration(
+                          labelText: 'Term',
+                          hintText: 'In years',
+                          border: OutlineInputBorder(
+                              borderRadius:
+                              BorderRadius.circular(minmumpadding))),
+                    )),
+              ),
+              Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(minmumpadding),
+                    child: DropdownButton<String>(
+                      hint: Text("Select a currencies"),
+                      value: currentcurrencies,
+                      onChanged: (String value) {
+                        setState(() {
+                          currentcurrencies = value;
+                        });
+                      },
+                      items: currencies
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    )),
+              ),
+            ]),
+            Row(children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(minmumpadding),
+                  child: RaisedButton(
+                    color: Theme
+                        .of(context)
+                        .accentColor,
+                    child: Text(
+                      'Calculate',
+                      style: textStyle,
                     ),
+                    onPressed: () {
+                      setState(() {
+                        result = calculateTheMoney();
+                      });
+                    },
                   ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(minmumpadding),
+                  child: RaisedButton(
+                    child: Text('Reset', style: textStyle),
+                    color: Colors.blueGrey,
+                    onPressed: () {
+                      setState(() {
+                        reset();
+                      });
 
-                ]
-            ),
-            Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.all(minmumpadding),
-                        child: RaisedButton(
-                          color: Theme.of(context).accentColor,
-                          child: Text('Calculate',style: textStyle,),
-
-                          onPressed:() {
-
-                          },
-                        ),
-                  ),),
-                  Expanded(
-                    child: Padding(
-                        padding: EdgeInsets.all(minmumpadding),
-                        child:RaisedButton(
-                          child: Text('Reset',style: textStyle),
-                          color: Colors.blueGrey
-                          ,
-                          onPressed:() {
-
-                          },
-                        ),
-                    ),
+                    },
                   ),
-
-                ]
-            ),
+                ),
+              ),
+            ]),
+            Padding(
+                padding: EdgeInsets.all(minmumpadding * 2),
+                child: Text(result, style: textStyle,)
+            )
           ],
         ),
       ),
@@ -140,5 +156,20 @@ class MoneyCalcState extends State<MoneyCalc> {
       child: image,
       margin: EdgeInsets.all(40.0),
     );
+  }
+
+  String calculateTheMoney() {
+    double principal = double.parse(principalController.text);
+    double rate = double.parse(rateController.text);
+    double term = double.parse(termController.text);
+    double investment = principal + (principal*rate*term)/100 ;
+    return 'after $term of years , your investment will be worth $investment $currentcurrencies';
+  }
+  void reset(){
+    principalController.text='';
+    termController.text="";
+    rateController.text="";
+    result="";
+    currentcurrencies=currencies[0];
   }
 }
